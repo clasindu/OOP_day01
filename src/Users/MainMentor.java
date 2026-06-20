3245bf72c8b2dedeab494cf624f6d483d38f4ead
clasindu <lasindura00@gmail.com>
2026-06-20 08:15:47 +0530

    create MainMentor class

---
+++ src/Users/MainMentor.java
@@ -0,0 +1,17 @@
+package Users;
+
+import java.util.ArrayList;
+
+public class MainMentor extends Mentor{
+
+    int percentage_Cut;
+
+    public MainMentor(String name , String email , ArrayList<String> subjects, int experience ,int pc){
+
+        super(name,email,subjects,experience);
+        this.percentage_Cut=pc;
+
+    }
+
+
+}
